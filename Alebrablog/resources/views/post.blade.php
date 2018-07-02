7a42c03cb5e6bac8a26ea5b66223e4662b77107d
danijelafaraguna <39788946+danijelafaraguna@users.noreply.github.com>
2018-07-02 09:26:54 +0200

    Update post.blade.php

--- Alebrablog/resources/views/post.blade.php
+++ Alebrablog/resources/views/post.blade.php
@@ -1,6 +1,6 @@
 @extends('Centaur::layout')
 
-@section('title') 
+@section('title')
 Algebra Blog | {{ $post->title }}
 @endsection
 
@@ -11,8 +11,41 @@ Algebra Blog | {{ $post->title }}
 		<small>Published: {{ \Carbon\Carbon::createFromTimestamp(strtotime($post->created_at))->diffForHumans() }}</small>
     </div>
 	<div class="row">
-		<div class="col-sm-12">
+		
 			{!! $post->content !!}
+		</div>
+	</div>
+	<div class="row">
+	
+			@if(Sentinel::check())
+				//Prikazi formu za komentiranje
+
+				<h2>Comments</h2>
+				@if (Auth::check())
+  				@include('includes.errors')
+  					{{ Form::open(['route' => ['comments.store'], 'method' => 'POST']) }}
+  			<p>{{ Form::textarea('body', old('body')) }}</p>
+  					{{ Form::hidden('post_id', $post->id) }}
+  			<p>{{ Form::submit('Send') }}</p>
+				{{ Form::close() }}
+				@endif
+				@forelse ($post->comments as $comment)
+  			<p>{{ $comment->user->name }} {{$comment->created_at}}</p>
+  			<p>{{ $comment->body }}</p>
+  			<hr>
+				@empty
+  		<p>This post has no comments</p>
+			<span>{{$post->comments->count()}} {{ str_plural('comment', $post->comments->count()) }}</span>
+			@endforelse
+
+			@else
+				Prikazi link za login
+			@endif
+		</div>
+	</div>
+	<div class="row">
+		
+			ipisi sve komentare $post->comment
 		</div>
 	</div>
 @endsection
